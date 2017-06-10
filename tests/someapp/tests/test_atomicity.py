48108ba625ff1138fac60dd93a3de6226bdb0f23
adrian <adriancarayol@gmail.com>
2017-06-10 20:15:06 +0200

    + Atomicity uses two database (neo4j + sqlite3)

---
+++ tests/someapp/tests/test_atomicity.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from neomodel import db, clear_neo4j_database
+from django.db import transaction
+from tests.someapp.models import Book, Library
+
+class AtomicityTestClass(TestCase):
+    def setUp(self):
+        clear_neo4j_database(db)
+
+    def test_create_object(self):
+        try:
+            with transaction.atomic(using="default"):
+                with db.transaction:
+                    sql_row = Library.objects.create(name="example")
+                    neo4j_node = Book(title="example", format="foo").save()
+                    raise Exception
+        except Exception as e:
+            pass
+
+        neo4j_q = Book.nodes.get_or_none(title="example")
+        sql_q = Library.objects.filter(name="example").first()
+
+        self.assertIsNone(neo4j_q)
+        self.assertIsNone(sql_q)
