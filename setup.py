fbced78008b67b18dd14943dd5e16268e4b30e15
zhongwenming <zhongwenming@xsyxsc.com>
2020-03-02 09:33:00 +0800

    Correct that python3.7 complains choices is kinda monotonic list#37

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_neomodel',
-    version='0.0.5',
+    version='0.0.4',
     description='Use Neo4j with Django!',
     long_description=open('README.rst').read(),
     author='Robin Edwards',
