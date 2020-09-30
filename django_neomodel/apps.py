f282375b2caccd15111530e5f8291f960498e195
Trent <trent@mainmast.io>
2020-09-30 14:11:55 +1000

    Update apps.py
    neomodel changed the configuration setting from MAX_POOL_SIZE to MAX_CONNECTION_POOL_SIZE

--- django_neomodel/apps.py
+++ django_neomodel/apps.py
@@ -11,10 +11,13 @@ class NeomodelConfig(AppConfig):
     verbose_name = 'Django neomodel'
 
     def read_settings(self):
+        # Support for older and newer versions of neomodel. If neither work, just use a sane default.
+        default_max_pool_size = getattr(config, 'MAX_POOL_SIZE', getattr(config, 'MAX_CONNECTION_POOL_SIZE', 50))
+        
         config.DATABASE_URL = getattr(settings, 'NEOMODEL_NEO4J_BOLT_URL', config.DATABASE_URL)
         config.FORCE_TIMEZONE = getattr(settings, 'NEOMODEL_FORCE_TIMEZONE', False)
         config.ENCRYPTED_CONNECTION = getattr(settings, 'NEOMODEL_ENCRYPTED_CONNECTION', True)
-        config.MAX_POOL_SIZE = getattr(settings, 'NEOMODEL_MAX_POOL_SIZE', config.MAX_POOL_SIZE)
+        config.MAX_POOL_SIZE = getattr(settings, 'NEOMODEL_MAX_POOL_SIZE', default_max_pool_size)
 
     def ready(self):
         self.read_settings()
