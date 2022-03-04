aa3ce05e410c940c4a3ca6c14a5d73f6919d6916
Robert Deibel <deibel.robert@googlemail.com>
2022-03-04 15:32:49 +0100

    add commands for removal & update of labels

---
+++ django_neomodel/management/commands/remove_labels.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+
+from ._remove_labels import remove_all_labels
+
+
+
+class Command(BaseCommand):
+    help = 'Install labels and constraints for your neo4j database'
+
+    def handle(self, *args, **options):
+        remove_all_labels(stdout=self.stdout)
+
+
