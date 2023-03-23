6ec5d73499b899b5eea66f7f170e3db376ce2f3e
Marius Conjeaud <rmcj@novonordisk.com>
2023-03-23 17:38:10 +0100

    Migrate build to pyproject.toml

--- django_neomodel/__init__.py
+++ django_neomodel/__init__.py
@@ -16,7 +16,7 @@ __author__ = 'Robin Edwards'
 __email__ = 'robin.ge@gmail.com'
 __license__ = 'MIT'
 __package__ = 'django_neomodel'
-__version__ = '0.0.6'
+__version__ = '0.0.8'
 
 
 default_app_config = 'django_neomodel.apps.NeomodelConfig'
