ce654bd0d388d3c24b74ea3d5b5d34a1c3a42dd8
naphal <naphal@naphal.net>
2023-06-18 14:38:30 -0400

    - Update pyproject.toml for >= Django 2.2 - Update urls.py to re_path

--- tests/urls.py
+++ tests/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns = [url(r"^admin/", admin.site.urls)] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [re_path(r"^admin/", admin.site.urls)] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
