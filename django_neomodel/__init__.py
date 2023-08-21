1cd1bcc92a178bfe4d7f0aee8426a862be1a6cb7
Marius Conjeaud <rmcj@novonordisk.com>
2023-08-21 14:33:57 +0200

    Update missing tag number

--- django_neomodel/__init__.py
+++ django_neomodel/__init__.py
@@ -16,7 +16,7 @@ __author__ = "Robin Edwards"
 __email__ = "robin.ge@gmail.com"
 __license__ = "MIT"
 __package__ = "django_neomodel"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 default_app_config = "django_neomodel.apps.NeomodelConfig"
